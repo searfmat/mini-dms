86ad8425d5b7bab397eed6a2de93837b255142ad
Matthew Searfass <mattsea1028@gmail.com>
2025-01-05 21:11:55 -0500

    Error managment for dashboard URL

--- MiniDMS/Pages/Index.cshtml.cs
+++ MiniDMS/Pages/Index.cshtml.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MiniDMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Web;
 
 namespace MiniDMS.Pages
 {
@@ -14,16 +18,39 @@ namespace MiniDMS.Pages
         {
             _context = context;
         }
-        public IList<Document> Documents { get; set; } = default!;
-        public string _folder;
+        public IList<Document> Documents { get; set; } = new List<Document>();
+        private string _folder;
         public async Task OnGetAsync(string folder)
         {
             _folder = folder;
 
-
             if (User.Identity.IsAuthenticated)
             {
-                Documents = await _context.Document.Where(x => x.Owner.Equals(User.Identity.Name)).ToListAsync();
+
+                IList<Document> userDocuments = await _context.Document.Where(x => x.Owner.Equals(User.Identity.Name)).ToListAsync();
+
+                if (string.IsNullOrEmpty(_folder))
+                {
+                    Documents = userDocuments.Where(x => x.ParentId.Equals(-1)).ToList();
+                }
+                else
+                {
+
+                        Document parent = userDocuments.SingleOrDefault(x => x.FileName.Equals(HttpUtility.UrlDecode(_folder)));
+                        if (parent != null)
+                        {
+                            Documents = parent.SubFiles.ToList();
+                        }
+                        else 
+                        {
+                        Response.Redirect("/Error");
+                    }
+
+                    
+
+
+                }
+                
             }
         }
     }
