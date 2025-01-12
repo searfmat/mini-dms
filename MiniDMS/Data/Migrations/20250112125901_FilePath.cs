1300f8a3ab7d4dab89da4443f92dd0d989346985
Matthew Searfass <mattsea1028@gmail.com>
2025-01-12 08:41:04 -0500

    Document upload

---
+++ MiniDMS/Data/Migrations/20250112125901_FilePath.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniDMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FilePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Document",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Document");
+        }
+    }
+}
